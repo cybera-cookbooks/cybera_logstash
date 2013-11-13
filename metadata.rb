0b007adc7db72eeadcdb9f7eb7549c0cdfcac4a0
Matt Delaney <matthew.l.delaney@gmail.com>
2013-11-13 11:12:11 -0700

    This is a stop-gap fix. We are re-adding the ubuntu user to the adm group... the logstash cookbook stomps the list of users in the group, so we have to rebuild it.... down the road it may make sense to abandon the community cookbook as it really has a lot of dependencies that we don't need (and likely never will).

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          'All rights reserved'
 name             'cybera_logstash'
 description      'Installs/Configures logstash for Cybera'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "rabbitmq"
 depends "elasticsearch"
--- recipes/agent.rb
+++ recipes/agent.rb
@@ -1,1 +1,10 @@
 include_recipe "logstash::agent"
+
+# Strangely the logstash recipe removes the 'ubuntu' user from the 'adm' group when 
+# it is set to use the group as the primary group for the logstash user. So here we 
+# re-add the ubuntu user
+group "adm" do
+  members "ubuntu"
+  action :modify
+  append true
+end
