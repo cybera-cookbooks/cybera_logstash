e242b40019da2cb6c2ab78fc76dd847804888e81
Matt Delaney <matthew.l.delaney@gmail.com>
2014-02-14 17:02:22 -0700

    fixed rabbitmq input

--- attributes/inputs.rb
+++ attributes/inputs.rb
@@ -3,7 +3,10 @@ default[:logstash][:config][:inputs][:rabbitmq][:enabled] = false
 default[:logstash][:config][:inputs][:rabbitmq][:template_path] = "#{inputs_path}/rabbitmq.erb"
 default[:logstash][:config][:inputs][:rabbitmq][:template_cookbook] = "cybera_logstash"
 default[:logstash][:config][:inputs][:rabbitmq][:variables][:host] = "localhost"
-default[:logstash][:config][:inputs][:rabbitmq][:variables][:port] = 9000
+default[:logstash][:config][:inputs][:rabbitmq][:variables][:port] = 5671
+default[:logstash][:config][:inputs][:rabbitmq][:variables][:user] = "logstashuser"
+default[:logstash][:config][:inputs][:rabbitmq][:variables][:password] = "logstashSuP3Rs3creT"
+default[:logstash][:config][:inputs][:rabbitmq][:variables][:ssl_enabled] = false
 
 default[:logstash][:config][:inputs][:syslog][:enabled] = false
 default[:logstash][:config][:inputs][:syslog][:template_path] = "#{inputs_path}/syslog.erb"
