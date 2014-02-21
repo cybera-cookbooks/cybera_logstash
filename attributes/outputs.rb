4f1a223580362f951bf607cdb9ae4e55c5a1dbf2
Micheal Jones <chealion@chealion.ca>
2014-02-21 23:13:26 +0000

    Add stdout

--- attributes/outputs.rb
+++ attributes/outputs.rb
@@ -5,10 +5,14 @@ default[:logstash][:config][:outputs][:elasticsearch][:template_cookbook] = "cyb
 default[:logstash][:config][:outputs][:elasticsearch][:variables][:host] = "localhost"
 
 default[:logstash][:config][:outputs][:rabbitmq][:enabled] = false
-default[:logstash][:config][:outputs][:rabbitmq][:template_path] = "#{inputs_path}/rabbitmq.erb"
+default[:logstash][:config][:outputs][:rabbitmq][:template_path] = "#{outputs_path}/rabbitmq.erb"
 default[:logstash][:config][:outputs][:rabbitmq][:template_cookbook] = "cybera_logstash"
 default[:logstash][:config][:outputs][:rabbitmq][:variables][:host] = "localhost"
 default[:logstash][:config][:outputs][:rabbitmq][:variables][:port] = 5671
 default[:logstash][:config][:outputs][:rabbitmq][:variables][:user] = "logstashuser"
 default[:logstash][:config][:outputs][:rabbitmq][:variables][:password] = "logstashSuP3Rs3creT"
 default[:logstash][:config][:outputs][:rabbitmq][:variables][:ssl_enabled] = false
+
+default[:logstash][:config][:outputs][:stdout][:variables][:debug] = false
+default[:logstash][:config][:outputs][:stdout][:variables][:codec] = "plain"
+default[:logstash][:config][:outputs][:stdout][:variables][:workers] = 1
